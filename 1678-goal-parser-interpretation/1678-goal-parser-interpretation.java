acba53e9252abdfbfb291ad4451c4f9a1c9cda21
Saumyam Rai <raisaumyam26@gmail.com>
2026-06-02 23:38:33 +0530

    Time: 2 ms (12.52%), Space: 43 MB (32.31%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String interpret(String command) {
+        String ans="";
+        for(int i=0; i<command.length(); i++){
+            if( command.charAt(i) == 'G' ){  ans+="G"; }
+            else if( command.charAt(i) == '(' && command.charAt(i+1) == ')' ) { ans+="o"; i++; }
+            else { ans+="al"; i+=3;}
+        }
+        return ans;
+    }
+}
