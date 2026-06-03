25bf7afd6f183a90bbd8e17492d2ef21ed5693fa
Saumyam Rai <raisaumyam26@gmail.com>
2026-06-03 21:55:19 +0530

    Time: 2 ms (100%), Space: 92.1 MB (46.55%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,23 +1,28 @@
 class Solution {
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        long ans=Long.MAX_VALUE;
-        long bestLand=Long.MAX_VALUE;
-        long bestWater=Long.MAX_VALUE;
+
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+
+        int ans = Integer.MAX_VALUE;
+        int bestLand = Integer.MAX_VALUE;
+        int bestWater = Integer.MAX_VALUE;
         for(int i = 0; i < landStartTime.length; i++) {
-            bestLand=Math.min(bestLand,(long)landStartTime[i]+landDuration[i]);
+            bestLand = Math.min(bestLand,
+                    landStartTime[i] + landDuration[i]);
         }
         for(int i = 0; i < waterStartTime.length; i++) {
-            bestWater=Math.min(bestWater,(long)waterStartTime[i]+waterDuration[i]);
+            bestWater = Math.min(bestWater, waterStartTime[i] + waterDuration[i]);
         }
         for(int i = 0; i < waterStartTime.length; i++) {
-            long finish=Math.max(bestLand,waterStartTime[i])+waterDuration[i];
-            ans = Math.min(ans, finish);
-        }
-        for(int i = 0; i < landStartTime.length; i++) {
-            long finish = Math.max(bestWater,landStartTime[i])+landDuration[i];
+            int finish = Math.max(bestLand, waterStartTime[i]) + waterDuration[i];
 
             ans = Math.min(ans, finish);
         }
-        return (int)ans;
+
+        for(int i = 0; i < landStartTime.length; i++) {
+             int finish=Math.max(bestWater, landStartTime[i])+landDuration[i];
+              ans = Math.min(ans, finish);
+             }
+        return ans;
     }
 }
