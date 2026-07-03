7d7ac7ddc3e8667022e1c588024f810c13c81450
Saumyam Rai <raisaumyam26@gmail.com>
2026-07-03 13:28:07 +0530

    Time: 4 ms (99.43%), Space: 53.6 MB (73.92%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -2,9 +2,8 @@ class Solution {
     public List<Integer> grayCode(int n) {
         ArrayList<Integer> arr=new ArrayList<>();
         int length=1<<n;
-        for(int i=0; i<length; i++){
-            int grey= i^(i>>1);
-            arr.add(grey);
+        for(int i=0; i<length; i++){ 
+            arr.add(i^(i>>1));
         }
         return arr;
     }
