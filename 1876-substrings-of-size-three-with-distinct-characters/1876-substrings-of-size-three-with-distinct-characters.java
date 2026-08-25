00960b1053ba7b3c2a3e8e32c8bc70ca93c9f1f9
Saumyam Rai <raisaumyam26@gmail.com>
2026-08-25 14:37:30 +0530

    Time: 0 ms (100%), Space: 42.9 MB (54.17%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -3,7 +3,6 @@ class Solution {
 
         char[] str = s.toCharArray();
         int count = 0;
-
         for (int i = 0; i < str.length - 2; i++) {
 
             if (str[i] != str[i + 1] && str[i] != str[i + 2]
